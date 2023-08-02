8928bd8b1583432313978fa1bd8a1545d66a0d74
Kashubyak Mykila <kashubyak514@gmail.com>
2023-08-02 22:54:51 +0300

    Lazy load is over

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 let options = {
 	root: null,
 	rootMargin: '5px',
-	threshold: 0.25,
+	threshold: 0.3,
 };
 let targets = document.querySelectorAll('._anim-items');
 let callback = function (entries, observer) {
@@ -87,36 +87,65 @@ function closeModal() {
 
 /// Lazy loading
 
+// const lazyImages = document.querySelectorAll('img[data-src]');
+// const map = document.querySelector('.map');
+// const windowHeight = document.documentElement.clientHeight;
+// let lazyImagesPosition = [];
+// if (lazyImages.length > 0) {
+// 	lazyImages.forEach(img => {
+// 		if (img.dataset.src || img.dataset.srcset) {
+// 			lazyImagesPosition.push(img.getBoundingClientRect().top + scrollY);
+// 			lazyScrollCheck();
+// 		}
+// 	});
+// }
+// window.addEventListener("scroll", function () {
+// 	console.log('work')
+// 	lazyScrollCheck();
+// });
+// function lazyScroll() {
+// 	console.log('work')
+// 	lazyScrollCheck();
+// }
+// function lazyScrollCheck() {
+// 	let imgIndex = lazyImagesPosition.findIndex(
+// 		item => scrollY > item - windowHeight
+// 	);
+// 	if (imgIndex >= 0) {
+// 		if (lazyImages[imgIndex].dataset.src) {
+// 			lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+// 			lazyImages[imgIndex].removeAttribute('data-src');
+// 		}
+// 		delete lazyImagesPosition[imgIndex];
+// 	}
+// }
+
+// Lazy load 
 const lazyImages = document.querySelectorAll('img[data-src]');
-const map = document.querySelector('.map');
-const windowHeight = document.documentElement.clientHeight;
-let lazyImagesPosition = [];
-if (lazyImages.length > 0) {
-	lazyImages.forEach(img => {
-		if (img.dataset.src || img.dataset.srcset) {
-			lazyImagesPosition.push(img.getBoundingClientRect().top + scrollY);
-			lazyScrollCheck();
-		}
+const lazyMaps = document.querySelectorAll('.map[data-map] > div[data-src]');
+
+const lazyLoad = (target, src) => {
+	const io = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				if (target.tagName === 'IMG') {
+					target.src = src; // Присваиваем src для изображения
+				} else if (target.tagName === 'DIV') {
+					target.innerHTML = `<iframe src="${src}" frameborder="0" style="border:0; width: 100%; height: 100%;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`; // Вставляем iframe для карты
+				}
+				observer.unobserve(target);
+			}
+		});
 	});
-}
-window.addEventListener("scroll", function () {
-	console.log('work')
-	lazyScrollCheck();
+
+	io.observe(target);
+};
+
+lazyImages.forEach(image => {
+	lazyLoad(image, image.getAttribute('data-src'));
 });
-function lazyScroll() {
-	console.log('work')
-	lazyScrollCheck();
-}
-function lazyScrollCheck() {
-	let imgIndex = lazyImagesPosition.findIndex(
-		item => scrollY > item - windowHeight
-	);
-	if (imgIndex >= 0) {
-		if (lazyImages[imgIndex].dataset.src) {
-			lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
-			lazyImages[imgIndex].removeAttribute('data-src');
-		}
-		delete lazyImagesPosition[imgIndex];
-	}
-}
+
+lazyMaps.forEach(map => {
+	lazyLoad(map, map.getAttribute('data-src'));
+});
 
